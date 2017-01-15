9f25713cc1a8f23a97d8c3b28231776110e599d7
Sergey <kovalchyks@gmail.com>
2017-01-16 00:44:02 +0200

    add EncodingFilter

---
+++ src/main/java/com/getprepared/filter/EncodingFilter.java
@@ -0,0 +1,25 @@
+package com.getprepared.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by koval on 15.01.2017.
+ */
+public class EncodingFilter implements Filter {
+
+    private static final String ENCODING = "UTF-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException { }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain filterChain) throws IOException, ServletException {
+        request.setCharacterEncoding(ENCODING);
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() { }
+}
