811cc356d53963d098f4854c15bde10783b0af9d
Sergey <kovalchyks@gmail.com>
2017-01-29 21:30:58 +0200

    add UserGuestFilterTest

---
+++ src/test/java/com/getprepared/filter/UserGuestFilterTest.java
@@ -0,0 +1,60 @@
+package com.getprepared.filter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by koval on 29.01.2017.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class UserGuestFilterTest {
+
+    @Mock
+    private HttpServletRequest request;
+
+    @Mock
+    private HttpServletResponse response;
+
+    @Mock
+    private FilterConfig config;
+
+    @Mock
+    private FilterChain chain;
+
+    private Filter filter;
+
+    @Before
+    public void setUp() {
+        filter = new UserGuestFilter();
+    }
+
+    @Test
+    public void requireInteractionsWithFilterConfig() throws Exception {
+        filter.init(config);
+        verify(config, times(2)).getInitParameter(anyString());
+    }
+
+    @Test
+    public void requireNoInteractionsDoFilterWhenNoSession() throws Exception {
+        filter.doFilter(request, response, chain);
+        verify(chain, never()).doFilter(eq(request), eq(response));
+    }
+
+    @Test
+    public void requireNoInteractionWithDestroy() {
+        filter.destroy();
+        verifyNoMoreInteractions(config);
+    }
+}
