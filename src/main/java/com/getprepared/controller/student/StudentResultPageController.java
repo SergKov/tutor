a74337c1f6b01c1687b20d9b5a667c2f3829370e
Sergey <kovalchyks@gmail.com>
2017-02-07 00:59:04 +0200

    add session constraint to StudentResultPageController

--- src/main/java/com/getprepared/controller/student/StudentResultPageController.java
+++ src/main/java/com/getprepared/controller/student/StudentResultPageController.java
@@ -1,12 +1,14 @@
 package com.getprepared.controller.student;
 
 import com.getprepared.constant.PageConstants;
-import com.getprepared.constant.WebConstants;
 import com.getprepared.controller.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import static com.getprepared.constant.PageConstants.REDIRECT;
+import static com.getprepared.constant.WebConstants.SESSION_ATTRIBUTES;
 
 /**
  * Created by koval on 06.02.2017.
@@ -15,7 +17,11 @@ public class StudentResultPageController extends AbstractController {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        request.getSession().getAttribute(WebConstants.SESSION_ATTRIBUTES.MARK);
+        if (request.getSession().getAttribute(SESSION_ATTRIBUTES.MARK) == null) {
+            response.sendRedirect(PageConstants.LINKS.NOT_FOUND);
+            return REDIRECT;
+        }
+        request.getSession().getAttribute(SESSION_ATTRIBUTES.MARK);
         return PageConstants.PAGES.STUDENT_GET_RESULT;
     }
 }
