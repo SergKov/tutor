39995b5c8c27617291a2e6119fae174aa9a61828
Sergey <kovalchyks@gmail.com>
2017-02-27 22:31:12 +0200

    edit ApplicationContext (trial version)

--- src/main/java/com/getprepared/infrastructure/context/ApplicationContext.java
+++ src/main/java/com/getprepared/infrastructure/context/ApplicationContext.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.HashMap;
@@ -34,8 +35,9 @@ public class ApplicationContext implements BeanFactory {
 
         for (Class clazz : classes) {
             if (clazz.isAnnotationPresent(Bean.class)) {
+                final Bean annotation = (Bean) clazz.getAnnotation(Bean.class);
                 try {
-                    map.put(clazz.getName(), clazz.newInstance());
+                    map.put(annotation.value(), clazz.newInstance());
                 } catch (InstantiationException | IllegalAccessException e) {
                     final String errorMsg = String.format("Failed to instantiate class %s", clazz.getName());
                     LOG.error(errorMsg, e);
