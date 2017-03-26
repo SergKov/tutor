37c28d9a938a62c446d463072def051cb00ad64e
Sergey <kovalchyks@gmail.com>
2017-03-26 21:44:05 +0300

    edit DataSourceUtils (remove nested interface)

--- src/main/java/com/getprepared/core/util/DataSourceUtils.java
+++ src/main/java/com/getprepared/core/util/DataSourceUtils.java
@@ -7,8 +7,6 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import static com.getprepared.core.util.DataSourceUtils.DataSourceCallback.call;
-
 /**
  * Created by koval on 08.01.2017.
  */
@@ -17,22 +15,12 @@ public final class DataSourceUtils {
 
     private static final Logger LOG = Logger.getLogger(DataSourceUtils.class);
 
-    public Connection getConnection(final DataSource ds)  {
-        return call(ds::getConnection, "Failed to get connection");
-    }
-
-    @FunctionalInterface
-    public interface DataSourceCallback<E> {
-
-        E call() throws SQLException;
-
-        static <E> E call(final DataSourceCallback<E> callback, final String errorMsg) {
-            try {
-                return callback.call();
-            } catch (final SQLException e) {
-                LOG.error(errorMsg, e);
-                throw new IllegalStateException(e);
-            }
+    public Connection getConnection(final DataSource ds) {
+        try {
+            return ds.getConnection();
+        } catch (final SQLException e) {
+            LOG.error("Failed to get connection", e);
+            throw new IllegalStateException(e);
         }
     }
 }
