5e86d26f98311c86b718202510c2d7f8ef44e214
Sergey <kovalchyks@gmail.com>
2017-03-30 20:27:23 +0300

    fix Page, Pageable

--- src/main/java/com/getprepared/persistence/database/pagination/Page.java
+++ src/main/java/com/getprepared/persistence/database/pagination/Page.java
@@ -1,36 +1,20 @@
 package com.getprepared.persistence.database.pagination;
 
-import org.apache.commons.collections4.CollectionUtils;
-
-import java.util.List;
-
 /**
  * Created by koval on 21.01.2017.
  */
-public class Page<E> implements Pageable<E>{
+public class Page implements Pageable {
 
-    private final List<E> content;
     private final Long totalCount;
-    private final Long pageIndex;
     private final Long pageSize;
 
-    public Page(List<E> content, Long totalCount, Long pageIndex, Long pageSize) {
-        this.content = content;
+    public Page(Long totalCount, Long pageSize) {
         this.totalCount = totalCount;
-        this.pageIndex = pageIndex;
         this.pageSize = pageSize;
     }
 
     public Long getPageCount() {
         return totalCount / pageSize;
-    }
-
-    public boolean isEmpty() {
-        return CollectionUtils.isEmpty(content);
-    }
-
-    public List<E> getContent() {
-        return content;
     }
 
     public Long getTotalCount() {
--- src/main/java/com/getprepared/persistence/database/pagination/Pageable.java
+++ src/main/java/com/getprepared/persistence/database/pagination/Pageable.java
@@ -1,17 +1,11 @@
 package com.getprepared.persistence.database.pagination;
 
-import java.util.List;
-
 /**
  * Created by koval on 30.03.2017.
  */
-public interface Pageable<E> {
+public interface Pageable {
 
     Long getPageCount();
-
-    boolean isEmpty();
-
-    List<E> getContent();
 
     Long getTotalCount();
 }
